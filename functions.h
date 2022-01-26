2ba77c131067593a4a670fd75c27569842e639d6
enestaner <enestaner963@gmail.com>
2022-01-26 16:22:04 +0300

    comments added

--- functions.h
+++ functions.h
@@ -12,14 +12,35 @@
   #define SCREEN_CLEAR system("clear");
 #endif
 
+//fills matrix(table) '0' and '1' or special chars depends on the table type
 void tableFiller(int *table, int length, char type, int bombAmount);
+
+//prints matrix on screen
 void tableGetter(int *table, char type, int length);
+
+//calculate bomb amount depends on difficulty
 int bombCounter(char difficulty, int length);
+
+//take coordinates then controls if there is a bomb or not
 int bombController(int row, int col, int *ptrGame, int length);
+
+//take coordinates, controls the box then put or remove flag
 void flagController(int row, int col, int *ptrFake, int length);
+
+//change all mine-boxes to 'x' and printing 'GAME OVER' part
 void gameFinisher(int *ptrGame, int *ptrFake, int length);
+
+//take coordinates and find surrounding coordinates
 void pointFinder(int row, int col, int length, int *list);
+
+//fills every box in 'bombTable' with surrounding mine amounts
 void bombScanner(int *ptrGame, int length, int row, int col, int *ptrFake, int newRow, int newCol, int a, int b);
+
+//take coordinates and prints surrounding mine amount (if amount == 0 then recalls itself)
 void bombAmountWriter(int *ptrGame, int length, int row, int col, int *ptrFake, int *list);
+
+//changing surrounding mine amounts '0' to '-' 
 void tableFixer(int *ptrFake, int length);
+
+//controls every turn if all safe-boxes opened or not
 int gameController(int *ptrFake, int *ptrGame, int length, int bomb);
